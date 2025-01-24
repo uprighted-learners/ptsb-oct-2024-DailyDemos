7a4fc694f1f2b687e9eafcb0a91ca67b146350ff
Tyler Lemke <tyleris@gmail.com>
2025-01-23 16:16:42 -0800

    pushing up vite example

---
+++ week14/vite-example/my-react-app/src/Header.jsx
@@ -0,0 +1,24 @@
+// function
+// define props if needed in parenthesis
+function Header({ text }) {
+  // add JSX to your function
+  return (
+    // add a fragment as a wrapper
+    <>
+      <nav>
+        {/* Add some extra links here */}
+        <a href="http://google.com">Google</a>
+
+        {/* prop example */}
+        {text}
+
+        {/* Add in a prop that is an array of link/title */}
+        {/* map over them to display all of the links in the nav */}
+        {/* https://react.dev/learn/rendering-lists#keeping-list-items-in-order-with-key */}
+      </nav>
+    </>
+  );
+}
+
+// export the function component
+export default Header;
